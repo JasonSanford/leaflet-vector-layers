58a1189a50ff464a83328e666c5c5f95b311214f
Jason Sanford <jason.sanford@mapmyfitness.com>
2012-05-30 00:05:13 -0600

    Some progress with events.

--- src/layer/Layer.js
+++ src/layer/Layer.js
@@ -560,11 +560,14 @@ lvector.Layer = lvector.Class.extend({
             
             // Loop through the return features
             for (var i = 0; i < data.features.length; i++) {
+
+                // Let's make this easier
+                var feature = data.features[i];
             
                 // if AGS layer type assigned "attributes" to "properties" to keep everything looking like GeoJSON Features
                 if (this instanceof lvector.EsriJSONLayer) {
-                    data.features[i].properties = data.features[i].attributes;
-                    delete data.features[i].attributes;
+                    feature.properties = feature.attributes;
+                    delete feature.attributes;
                 }
 
                 // All objects are assumed to be false until proven true (remember COPS?)
@@ -577,7 +580,7 @@ lvector.Layer = lvector.Class.extend({
                     for (var i2 = 0; i2 < this._vectors.length; i2++) {
                     
                         // Does the "uniqueField" property for this feature match the feature on the map
-                        if (data.features[i].properties[this.options.uniqueField] == this._vectors[i2].properties[this.options.uniqueField]) {
+                        if (feature.properties[this.options.uniqueField] == this._vectors[i2].properties[this.options.uniqueField]) {
                             // The feature is already on the map
                             onMap = true;
                             
@@ -585,21 +588,21 @@ lvector.Layer = lvector.Class.extend({
                             if (this.options.dynamic) {
                             
                                 // The feature's geometry might have changed, let's check.
-                                if (this._getGeometryChanged(this._vectors[i2].geometry, data.features[i].geometry)) {
+                                if (this._getGeometryChanged(this._vectors[i2].geometry, feature.geometry)) {
                                     
                                     // Check to see if it's a point feature, these are the only ones we're updating for now
-                                    if (!isNaN(data.features[i].geometry.coordinates[0]) && !isNaN(data.features[i].geometry.coordinates[1])) {
-                                        this._vectors[i2].geometry = data.features[i].geometry;
+                                    if (!isNaN(feature.geometry.coordinates[0]) && !isNaN(feature.geometry.coordinates[1])) {
+                                        this._vectors[i2].geometry = feature.geometry;
                                         this._vectors[i2].vector.setLatLng(new L.LatLng(this._vectors[i2].geometry.coordinates[1], this._vectors[i2].geometry.coordinates[0]));
                                     }
                                     
                                 }
                                 
-                                var propertiesChanged = this._getPropertiesChanged(this._vectors[i2].properties, data.features[i].properties);
+                                var propertiesChanged = this._getPropertiesChanged(this._vectors[i2].properties, feature.properties);
                                 
                                 if (propertiesChanged) {
-                                    var symbologyPropertyChanged = this._getPropertyChanged(this._vectors[i2].properties, data.features[i].properties, this.options.symbology.property);
-                                    this._vectors[i2].properties = data.features[i].properties;
+                                    var symbologyPropertyChanged = this._getPropertyChanged(this._vectors[i2].properties, feature.properties, this.options.symbology.property);
+                                    this._vectors[i2].properties = feature.properties;
                                     if (this.options.popupTemplate) {
                                         this._setPopupContent(this._vectors[i2]);
                                     }
@@ -639,48 +642,72 @@ lvector.Layer = lvector.Class.extend({
                     
                     if (this instanceof lvector.GeoJSONLayer) {
                     // Convert GeoJSON to Leaflet vector (Point, Polyline, Polygon)
-                        var vector_or_vectors = this._geoJsonGeometryToLeaflet(data.features[i].geometry, this._getFeatureVectorOptions(data.features[i]));
-                        data.features[i][vector_or_vectors instanceof Array ? "vectors" : "vector"] = vector_or_vectors;
+                        var vector_or_vectors = this._geoJsonGeometryToLeaflet(feature.geometry, this._getFeatureVectorOptions(feature));
+                        feature[vector_or_vectors instanceof Array ? "vectors" : "vector"] = vector_or_vectors;
                     } else if (this instanceof lvector.EsriJSONLayer) {
                         // Convert Esri JSON to Google Maps vector (Point, Polyline, Polygon)
-                        var vector_or_vectors = this._esriJsonGeometryToLeaflet(data.features[i].geometry, this._getFeatureVectorOptions(data.features[i]));
-                        data.features[i][vector_or_vectors instanceof Array ? "vectors" : "vector"] = vector_or_vectors;
+                        var vector_or_vectors = this._esriJsonGeometryToLeaflet(feature.geometry, this._getFeatureVectorOptions(feature));
+                        feature[vector_or_vectors instanceof Array ? "vectors" : "vector"] = vector_or_vectors;
                     }
                     
                     // Show the vector or vectors on the map
-                    if (data.features[i].vector) {
-                        this.options.map.addLayer(data.features[i].vector);
-                    } else if (data.features[i].vectors && data.features[i].vectors.length) {
-                        for (var i3 = 0; i3 < data.features[i].vectors.length; i3++) {
-                            this.options.map.addLayer(data.features[i].vectors[i3]);
+                    if (feature.vector) {
+                        this.options.map.addLayer(feature.vector);
+                    } else if (feature.vectors && feature.vectors.length) {
+                        for (var i3 = 0; i3 < feature.vectors.length; i3++) {
+                            this.options.map.addLayer(feature.vectors[i3]);
                         }
                     }
                     
                     // Store the vector in an array so we can remove it later
-                    this._vectors.push(data.features[i]);
-                    
-                    if (this.options.popupTemplate) {
-                        
-                        var me = this;
-                        var feature = data.features[i];
-                        
-                        this._setPopupContent(feature);
-                        
-                        (function(feature){
-                            if (feature.vector) {
-                                feature.vector.on("click", function(event) {
-                                    me._showPopup(feature, event);
-                                });
-                            } else if (feature.vectors) {
-                                for (var i3 = 0, len = feature.vectors.length; i3 < len; i3++) {
-                                    feature.vectors[i3].on("click", function(event) {
+                    this._vectors.push(feature);
+
+                    var me = this;
+
+                    (function(feature) {
+                        if (me.options.events) {
+                            var eventTypes = ["mouseover", "mouseout", "click"];
+                            for (var type in eventTypes) {
+                                (function(type) {
+                                    if (me.options.events[eventTypes[type]]) {
+                                        if (feature.vector) {
+                                            feature.vector.on(eventTypes[type], function(event) {
+                                                me.options.events[eventTypes[type]](event, feature.vector, feature.properties);
+                                            });
+                                        } else if (feature.vectors) {
+                                            for (var i3 = 0; i3 < feature.vectors.length; i3++) {
+                                                (function(it) {
+                                                    feature.vectors[it].on(eventTypes[type], function(event) {
+                                                        me.options.events[eventTypes[type]](event, feature.vectors[it], feature.properties);
+                                                    });
+                                                }(i3));
+                                            }
+                                        }
+                                    }
+                                }(type));
+                            }
+                        }
+                        if (me.options.popupTemplate) {
+                            
+                            me._setPopupContent(feature);
+                            
+                            (function(feature) {
+                                if (feature.vector) {
+                                    feature.vector.on("click", function(event) {
                                         me._showPopup(feature, event);
                                     });
+                                } else if (feature.vectors) {
+                                    for (var i3 = 0, len = feature.vectors.length; i3 < len; i3++) {
+                                        feature.vectors[i3].on("click", function(event) {
+                                            me._showPopup(feature, event);
+                                        });
+                                    }
                                 }
-                            }
-                        }(feature));
-                        
-                    }
+                            }(feature));
+                            
+                        }
+                    }(feature));
+
                 
                 }
                 
